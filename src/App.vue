f938ea2767c30d7d57154e393c5abfc85db91f32
Toniogabe <antoniogabriel@outlook.com>
2023-02-02 19:00:27 -0500

    feat: add rotate action

--- src/App.vue
+++ src/App.vue
@@ -41,17 +41,29 @@ let colors = [
     return `rounded-${randomFromArray(rounds)}-full`
   }
 
+  function rotate (item) {
+    let object = document.getElementById(item)
+    object.style.transform = 'rotate(90deg)'
+  }
+
 
 </script>
 
 <template>
-  <main class="">
-    <div class="grid w-full h-screen" style="grid-template-columns: repeat(50, minmax(0, 1fr));">
+  <main>
+    <div class="grid w-full h-screen" style="grid-template-columns: repeat(25, minmax(0, 1fr));">
       <div
-        class="h-full w-full"
-        :class="getColor(), getRounded()"
-        v-for="item in 1000"
-      ></div>
+        class="h-20 w-20 hover:scale-150 hover:shadow-lg transition-all ease-in-out"
+        :class="getColor()"
+        v-for="item in 350" :ref="item"
+        :id="item"
+        @click="rotate(item)"
+      >
+        <div
+          class="h-full w-full"
+          :class="getColor(), getRounded()"
+        ></div>
+      </div>
     </div>
   </main>
 </template>
